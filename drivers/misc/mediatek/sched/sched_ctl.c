5e9f500b45eae13343ee6721a436566bc1d40046
QKIvan <ivan33@163.com>
2024-05-04 15:29:22 -0400

    Revert "sched: refine the get efuse flow"
    * Very strange,it always tips missing symbols and breaks MT6873/6875 devices.To pass build,revert this commit for now.
    * Test devices: atom (MT6875)
    
    ld.lld: error: undefined symbol: init_efuse_info
    >>> referenced by core.c
    >>>               kernel/sched/core.o:(sched_init_smp) in archive built-in.o
    
    Signed-off-by: QKIvan <ivan33@163.com>

--- drivers/misc/mediatek/sched/sched_ctl.c
+++ drivers/misc/mediatek/sched/sched_ctl.c
@@ -566,14 +566,6 @@ out:
 }
 #endif
 
-#ifdef CONFIG_MACH_MT6873
-static int efuse_aware_big_thermal;
-void __init init_efuse_info(void)
-{
-	efuse_aware_big_thermal = (get_devinfo_with_index(7) & 0xFF) == 0x30;
-}
-#endif
-
 int select_task_prefer_cpu(struct task_struct *p, int new_cpu)
 {
 	int task_prefer;
